2653eae41080e7f3fadb9dba6714f33ccb982a1a
caiomarqs <caiomarques1412@gmail.com>
2020-10-15 16:12:33 -0300

    config for vscode

--- deps.ts
+++ deps.ts
@@ -1,4 +1,2 @@
-import { Status } from "https://deno.land/std@0.73.0/http/http_status.ts"
-
 export * as oak from 'https://deno.land/x/oak/mod.ts'
 export { Status, STATUS_TEXT } from "https://deno.land/std@0.73.0/http/http_status.ts"
